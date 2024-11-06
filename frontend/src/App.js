9abeccfee2e4487d8e05756f30375a9a5ed84021
unknown <rm542872@dal.ca>
2024-11-06 10:53:50 -0400

    Modified App.js to include the routes and package.json and package-lock.json to install firebase to application to resolve conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,26 @@ import {
 import { AuthProvider } from "./context/AuthContext";
 import Login from "../src/components/authentication/Login";
 import Registeration from "../src/components/authentication/Registeration";
+import Home from "../src/components/Home";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import ProtectedRoute from "./utility/RouteProtection";
 import Assistant from "./components/assistant/Assistant";
 
 const App = () => {
 	return (
 		<AuthProvider>
 			<Router>
+			    <Navbar />
 				<Routes>
 					<Route path='/login' element={<Login />} />
 					<Route path='/register' element={<Registeration />} />
 					<Route path='/' element={<Navigate to='/login' />} />
 					<Route path='/assistant' element={<Assistant />} />
+					<Route
+						path='/dashboard'
+						element={<ProtectedRoute element={<Dashboard />} />}
+					/>
 				</Routes>
 			</Router>
 		</AuthProvider>
