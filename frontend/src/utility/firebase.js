b708428cf00ff80025c0c83dc9b79ab846cf9313
alishan <al459703@dal.ca>
2024-11-30 21:53:51 -0400

    changed database for login and registration log collection

--- frontend/src/utility/firebase.js
+++ frontend/src/utility/firebase.js
@@ -1,18 +1,18 @@
 // firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiSqqPFFkqLN6A-gnlTdXg_9raR8Kqo7s",
-  authDomain: "serverless-439419.firebaseapp.com",
-  projectId: "serverless-439419",
-  storageBucket: "serverless-439419.appspot.com",
-  messagingSenderId: "904524447215",
-  appId: "1:904524447215:web:56b168372421a2f51b4a1a"
+	apiKey: "AIzaSyAiSqqPFFkqLN6A-gnlTdXg_9raR8Kqo7s",
+	authDomain: "serverless-439419.firebaseapp.com",
+	projectId: "serverless-439419",
+	storageBucket: "serverless-439419.appspot.com",
+	messagingSenderId: "904524447215",
+	appId: "1:904524447215:web:56b168372421a2f51b4a1a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app,"chat-data");
+const db = getFirestore(app);
 
 export { db };
