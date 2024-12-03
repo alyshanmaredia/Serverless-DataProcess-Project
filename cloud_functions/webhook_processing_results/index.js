fceae3a6915d859f7f831909ecca0cfa681f8385
Harsh Vaghasiya <harsh.vaghasiya@dal.ca>
2024-12-03 16:49:27 -0400

    updated webhook cloud function code

--- cloud_functions/webhook_processing_results/index.js
+++ cloud_functions/webhook_processing_results/index.js
@@ -1,10 +1,21 @@
+import axios from 'axios';
+
 export async function getResults(req, res) {
     try {
-        // const userMessage = req.body.text;
-        // const referanceCode = req.body.intentInfo.parameters.referance_code.resolvedValue;
         const referanceCode = req.body.intentInfo.parameters.referance_code.originalValue;
-        const responseMessage = "Testing file processing results for referance code: "+referanceCode;
-        responseMessage += "\n\nIs there anything else I can assist you with Today?";
+        let responseMessage = "There is a problem accessing the results for this code.";
+        
+
+        try {
+          const url = "https://pg70ny2xv0.execute-api.us-east-1.amazonaws.com/dev/getResult?process_id=" + referanceCode;
+          const prossingResault = await axios.get(url);
+          const filePath = prossingResault.data?.FileKey;
+          const s3Url = `https://dataprocessorinputdal1.s3.amazonaws.com/${encodeURIComponent(filePath)}`;
+          responseMessage = s3Url;
+        } catch (axiosError) {
+          console.error('Error fetching QDP agent:', axiosError.message);
+          responseMessage = 'Failed to get processing results for this code. Please try again later.'
+        }
         res.json({ 
           fulfillment_response: {
             messages: [
